33eb73c01a06d490a338af675a5cc9dfa4d04d1f
Sam <swheeler111@gmail.com>
2018-08-22 16:49:30 -0400

    add load_schema back

--- tap_pardot/schemas.py
+++ tap_pardot/schemas.py
@@ -22,6 +22,10 @@ def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
 
+def load_schema(tap_stream_id):
+    path = "schemas/{}.json".format(tap_stream_id)
+    return utils.load_json(get_abs_path(path))
+
 def get_stream_from_catalog(stream_id, catalog):
     streams = catalog['streams']
     for s in streams:
